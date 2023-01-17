e4d2c9f2c0192d9e6dc4c806d3dd2509932530cf
Tinuki <mail@tinuki.xyz>
2023-01-17 10:56:40 +0100

    Cleanup unnecessary comments
    some comments were there for purpose of testing

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 sudo docker run -it --rm -e MINECRAFT_VERSION="1.19" -v /home/tinuki/Documents/projects/minecraft-map/server/:/home/minecraft/server/ -v /home/tinuki/Documents/projects/minecraft-map/data/:/home/minecraft/.minecraft/ -v /home/tinuki/Documents/projects/minecraft-map/output/:/home/minecraft/output/ overviewer /bin/bash
-# /home/minecraft/.minecraft/run.sh 
-
-# sudo docker run -it --rm -e MINECRAFT_VERSION="1.19" -v /home/tinuki/Documents/projects/minecraft-map/data/:/home/minecraft/.minecraft/ -v /home/tinuki/Documents/projects/minecraft-map/output/:/home/minecraft/output/ mide/minecraft-overviewer:latest /bin/bash
 # /home/minecraft/.minecraft/run.sh
